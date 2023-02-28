f6bad3a2a7865ec823487d8b0818229964d4ea56
Mohamed El Mehdi Rahmoun <39870060+mahdimedRhm@users.noreply.github.com>
2023-02-28 13:49:08 -0500

    fix: excute commands with sudo

--- snort-set-up.sh
+++ snort-set-up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # changing the sources.list
-mv /etc/apt/sources.list /etc/apt/sources.list.bak
-find /var/lib/apt/lists -type f -exec rm -rf {} \;
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo find /var/lib/apt/lists -type f -exec rm -rf {} \;
 # download ubuntu source.list
-wget https://gist.githubusercontent.com/ishad0w/788555191c7037e249a439542c53e170/raw/3822ba49241e6fd851ca1c1cbcc4d7e87382f484/sources.list -O /etc/apt/sources.list
+sudo wget https://gist.githubusercontent.com/ishad0w/788555191c7037e249a439542c53e170/raw/3822ba49241e6fd851ca1c1cbcc4d7e87382f484/sources.list -O /etc/apt/sources.list
 
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
@@ -14,15 +14,15 @@ sudo apt update
 sudo apt install snort
 
 # return the sources list of kali
-find /var/lib/apt/lists -type f -exec rm -rf {} \;
-mv /etc/apt/sources.list /etc/apt/ubuntu_sources.list
-mv /etc/apt/sources.list.bak /etc/apt/sources.list
+sudo find /var/lib/apt/lists -type f -exec rm -rf {} \;
+sudo mv /etc/apt/sources.list /etc/apt/ubuntu_sources.list
+sudo mv /etc/apt/sources.list.bak /etc/apt/sources.list
 
-mkdir /etc/snort /etc/snort/rules /var/log/snort
-touch /etc/snort/snort.conf /etc/snort/rules/icmp.rules
+sudo mkdir /etc/snort /etc/snort/rules /var/log/snort
+sudo touch /etc/snort/snort.conf /etc/snort/rules/icmp.rules
 
-echo 'include /etc/snort/rules/icmp.rules' > /etc/snort/snort.conf
-echo 'alert icmp any any -> any any (msg:"ICMP Packet"; sid:477; rev:3;)' > /etc/snort/rules/icmp.rules
+sudo echo 'include /etc/snort/rules/icmp.rules' > /etc/snort/snort.conf
+sudo echo 'alert icmp any any -> any any (msg:"ICMP Packet"; sid:477; rev:3;)' > /etc/snort/rules/icmp.rules
 # on this format => <Rule Actions> <Protocol> <Source IP Address> <Source Port> <Direction Operator> <Destination IP Address> <Destination > (rule options)
 
-echo 'run snort with this following command: snort -c /etc/snort/snort.conf -l /var/log/snort/'
+sudo echo 'run snort with this following command: snort -c /etc/snort/snort.conf -l /var/log/snort/'
